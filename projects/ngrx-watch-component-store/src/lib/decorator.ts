fc7145e9d0a5de081fdd36f08d41e673b17f0e6f
Odonno <bottiau.david@laposte.net>
2022-11-10 14:48:17 +0100

    ✨ display prevState on each log

--- projects/ngrx-watch-component-store/src/lib/decorator.ts
+++ projects/ngrx-watch-component-store/src/lib/decorator.ts
@@ -1,5 +1,6 @@
 import { ComponentStore } from '@ngrx/component-store';
-import { tap } from 'rxjs/operators';
+import { merge } from 'rxjs';
+import { bufferCount, take, tap } from 'rxjs/operators';
 
 /**
  * Decorator used to log state of a @ngrx/component-store
@@ -9,11 +10,36 @@ export function WatchComponentState() {
   return <T extends { new (...args: any[]): ComponentStore<any> }>(
     target: T
   ) => {
+    const name = target.name;
+
     return class extends target {
       watchState = this.effect(() => {
-        return this.state$.pipe(
-          tap((state) => console.log(target.name, state))
+        const logInitialState$ = this.state$.pipe(
+          take(1),
+          tap((state) => {
+            const obj = {
+              name,
+              state,
+            };
+
+            console.log(obj);
+          })
         );
+
+        const logDiff$ = this.state$.pipe(
+          bufferCount(2, 1),
+          tap(([prevState, state]) => {
+            const obj = {
+              name,
+              prevState,
+              state,
+            };
+
+            console.log(obj);
+          })
+        );
+
+        return merge(logInitialState$, logDiff$);
       });
     };
   };
